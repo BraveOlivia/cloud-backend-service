1f000bd9fcc16361bd18ab8992076d202810a835
Olivia <zhouxuanolivia@gmail.com>
2026-03-01 13:48:36 +1000

    feat: dockerize app and add docker compose for postgres

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://oliviazhou@localhost:5435/events_db"
-
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://oliviazhou@localhost:5435/events_db"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
